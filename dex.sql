0ae8287ae683288813b5e8650f934d745bcf6b2a
Livspect <36874572+livspect@users.noreply.github.com>
2019-06-23 23:44:27 +0900

    update dex.sql

--- dex.sql
+++ dex.sql
@@ -4,90 +4,129 @@
 
 -- ------------------------------------------------------
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8mb4 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dex`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `asset`
 --
 
-DROP TABLE IF EXISTS `asset`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `asset` (
-  `id_asset` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_asset` int(10) UNSIGNED NOT NULL,
   `id_waves` varchar(255) NOT NULL,
   `id_bcm` varchar(255) DEFAULT NULL,
   `symbol` varchar(20) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
-  `decimals` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id_asset`),
-  UNIQUE KEY `id_waves` (`id_waves`)
-) ENGINE=InnoDB AUTO_INCREMENT=5932740 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `decimals` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `config`
 --
 
-DROP TABLE IF EXISTS `config`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `config` (
-  `id_config` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_config` int(10) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
-  `value` bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (`id_config`)
+  `value` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `rate`
 --
 
-DROP TABLE IF EXISTS `rate`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `rate` (
-  `id_asset_source` int(10) unsigned NOT NULL,
-  `id_asset_target` int(10) unsigned NOT NULL,
-  `id_source` int(10) unsigned NOT NULL,
-  `rate` bigint(20) unsigned NOT NULL,
-  `volume_source` bigint(20) unsigned DEFAULT NULL,
-  `volume_target` bigint(20) unsigned DEFAULT NULL,
-  `timestamp` datetime NOT NULL,
-  KEY `id_source` (`id_source`),
-  KEY `id_asset_source` (`id_asset_source`)
+  `id_asset_source` int(10) UNSIGNED NOT NULL,
+  `id_asset_target` int(10) UNSIGNED NOT NULL,
+  `id_source` int(10) UNSIGNED NOT NULL,
+  `rate` bigint(20) UNSIGNED NOT NULL,
+  `volume_source` bigint(20) UNSIGNED DEFAULT NULL,
+  `volume_target` bigint(20) UNSIGNED DEFAULT NULL,
+  `timestamp` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `status`
 --
 
-DROP TABLE IF EXISTS `status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `status` (
-  `id_status` int(10) unsigned NOT NULL,
-  `status` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id_status`)
+  `id_status` int(10) UNSIGNED NOT NULL,
+  `status` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`id_status`, `status`) VALUES
+(1, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `asset`
+--
+ALTER TABLE `asset`
+  ADD PRIMARY KEY (`id_asset`),
+  ADD UNIQUE KEY `id_waves` (`id_waves`);
+
+--
+-- Indexes for table `config`
+--
+ALTER TABLE `config`
+  ADD PRIMARY KEY (`id_config`);
+
+--
+-- Indexes for table `rate`
+--
+ALTER TABLE `rate`
+  ADD KEY `id_source` (`id_source`),
+  ADD KEY `id_asset_source` (`id_asset_source`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id_status`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `asset`
+--
+ALTER TABLE `asset`
+  MODIFY `id_asset` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5932740;
+
+--
+-- AUTO_INCREMENT for table `config`
+--
+ALTER TABLE `config`
+  MODIFY `id_config` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

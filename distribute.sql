9029e22682403e3b1a2281a042ba6e5399192045
Livspect <36874572+livspect@users.noreply.github.com>
2019-06-23 20:53:52 +0900

    update distribtue.sql

--- distribute.sql
+++ distribute.sql
@@ -1,8 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.16, for Linux (x86_64)
---
--- Host: localhost    Database: distribute
 -- ------------------------------------------------------
--- Server version	8.0.16
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -206,5 +202,3 @@ CREATE TABLE `transaction` (
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-06-23 13:48:08

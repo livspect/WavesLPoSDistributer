f6ba08d719cdb393582493871d90bdb4aa2fbff4
Livspect <36874572+livspect@users.noreply.github.com>
2019-06-23 23:42:27 +0900

    update airdrop.sql

--- airdrop.sql
+++ airdrop.sql
@@ -4,131 +4,213 @@
 
 -- ------------------------------------------------------
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8mb4 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `airdrop`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `address`
 --
 
-DROP TABLE IF EXISTS `address`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `address` (
-  `id_address` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_address` int(10) UNSIGNED NOT NULL,
   `address` varchar(36) NOT NULL,
-  `excluded` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id_address`)
-) ENGINE=InnoDB AUTO_INCREMENT=894 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `excluded` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `asset`
 --
 
-DROP TABLE IF EXISTS `asset`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `asset` (
-  `id_asset` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `id` varchar(64) NOT NULL,
-  PRIMARY KEY (`id_asset`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id_asset` int(10) UNSIGNED NOT NULL,
+  `id` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `asset`
+--
+
+INSERT INTO `asset` (`id_asset`, `id`) VALUES
+(1, 'your_asset_id');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `balance`
 --
 
-DROP TABLE IF EXISTS `balance`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `balance` (
-  `id_address` int(10) unsigned NOT NULL,
-  `balance` bigint(20) unsigned NOT NULL,
-  `timestamp` datetime NOT NULL,
-  KEY `id_address` (`id_address`)
+  `id_address` int(10) UNSIGNED NOT NULL,
+  `balance` bigint(20) UNSIGNED NOT NULL,
+  `timestamp` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `config`
 --
 
-DROP TABLE IF EXISTS `config`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `config` (
-  `id_config` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_config` int(10) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
-  `value` bigint(10) unsigned NOT NULL,
-  PRIMARY KEY (`id_config`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `value` bigint(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `config`
+--
+
+INSERT INTO `config` (`id_config`, `name`, `value`) VALUES
+(1, 'Airdrop amount', 0),
+(2, 'Airdrop fee asset', 0),
+(3, 'Airdrop fee amount', 100000);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `distribution`
 --
 
-DROP TABLE IF EXISTS `distribution`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `distribution` (
-  `id_distribution` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `timestamp` datetime NOT NULL,
-  PRIMARY KEY (`id_distribution`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id_distribution` int(10) UNSIGNED NOT NULL,
+  `timestamp` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `journal_line`
 --
 
-DROP TABLE IF EXISTS `journal_line`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `journal_line` (
-  `id_journal_line` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `id_journal_line_type` int(10) unsigned NOT NULL,
-  `id_distribution` int(10) unsigned DEFAULT NULL,
-  `id_address` int(10) unsigned NOT NULL,
-  `amount` bigint(20) unsigned NOT NULL,
-  `id_asset` int(10) unsigned NOT NULL,
+  `id_journal_line` int(10) UNSIGNED NOT NULL,
+  `id_journal_line_type` int(10) UNSIGNED NOT NULL,
+  `id_distribution` int(10) UNSIGNED DEFAULT NULL,
+  `id_address` int(10) UNSIGNED NOT NULL,
+  `amount` bigint(20) UNSIGNED NOT NULL,
+  `id_asset` int(10) UNSIGNED NOT NULL,
   `id_tx` varchar(255) DEFAULT NULL,
-  `tx_status` tinyint(1) unsigned DEFAULT NULL,
-  `id_journal_line_ref` int(10) unsigned DEFAULT NULL,
-  `timestamp` datetime NOT NULL,
-  PRIMARY KEY (`id_journal_line`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `tx_status` tinyint(1) UNSIGNED DEFAULT NULL,
+  `id_journal_line_ref` int(10) UNSIGNED DEFAULT NULL,
+  `timestamp` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `status`
 --
 
-DROP TABLE IF EXISTS `status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `status` (
-  `id_status` int(1) unsigned NOT NULL,
-  `status` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id_status`)
+  `id_status` int(1) UNSIGNED NOT NULL,
+  `status` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`id_status`, `status`) VALUES
+(1, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`id_address`);
+
+--
+-- Indexes for table `asset`
+--
+ALTER TABLE `asset`
+  ADD PRIMARY KEY (`id_asset`);
+
+--
+-- Indexes for table `balance`
+--
+ALTER TABLE `balance`
+  ADD KEY `id_address` (`id_address`);
+
+--
+-- Indexes for table `config`
+--
+ALTER TABLE `config`
+  ADD PRIMARY KEY (`id_config`);
+
+--
+-- Indexes for table `distribution`
+--
+ALTER TABLE `distribution`
+  ADD PRIMARY KEY (`id_distribution`);
+
+--
+-- Indexes for table `journal_line`
+--
+ALTER TABLE `journal_line`
+  ADD PRIMARY KEY (`id_journal_line`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id_status`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `id_address` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `asset`
+--
+ALTER TABLE `asset`
+  MODIFY `id_asset` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `config`
+--
+ALTER TABLE `config`
+  MODIFY `id_config` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `distribution`
+--
+ALTER TABLE `distribution`
+  MODIFY `id_distribution` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `journal_line`
+--
+ALTER TABLE `journal_line`
+  MODIFY `id_journal_line` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
